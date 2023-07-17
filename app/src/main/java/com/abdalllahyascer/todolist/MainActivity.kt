4e428f9469152b855a5aeb1a332ea464d9769db6
Abdallah Yasser <abdallahyasser1277@gmail.com>
2023-07-17 23:36:17 +0200

    add splash screen with logo

--- app/src/main/java/com/abdalllahyascer/todolist/MainActivity.kt
+++ app/src/main/java/com/abdalllahyascer/todolist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.abdalllahyascer.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.abdalllahyascer.todolist.adapters.TodoListAdapter
 import com.abdalllahyascer.todolist.databinding.ActivityMainBinding
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() ,TaskItemClickListener{
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1500)
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding= ActivityMainBinding.inflate(layoutInflater)
